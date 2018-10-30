5994dbc57436ea383e6b0c66bc21b1ff93d57b05
kvinkel <kvinkel@hotmail.com>
2018-10-30 14:53:14 +0100

    Add allowed directions

--- src/edu/sdu/woz/Game.java
+++ src/edu/sdu/woz/Game.java
@@ -1,6 +1,6 @@
 package edu.sdu.woz;
 
-import static edu.sdu.woz.Direction.EAST;
+import static edu.sdu.woz.Direction.*;
 import edu.sdu.woz.facade.IFacade;
 
 import java.awt.*;
@@ -20,30 +20,29 @@ public class Game {
         // Entrance
         putRoom(new EmptyRoom(this, new Point(0,0),
                 "You find yourself standing in a grand entrance. There's a hallway to the north."
-        ));
-
+        ).setDirections(EAST,WEST));
         // End Room~
         putRoom(new EmptyRoom(this, new Point(-1,0),"End Room").setDirections(EAST));
         // Dining Room~
-        putRoom(new EmptyRoom(this, new Point(1,0),"Dining Room"));
+        putRoom(new EmptyRoom(this, new Point(1,0),"Dining Room").setDirections(NORTH,EAST,SOUTH,WEST));
         // Room
-        putRoom(new EmptyRoom(this, new Point(2,0),"Room"));
+        putRoom(new EmptyRoom(this, new Point(2,0),"Room").setDirections(EAST,SOUTH,WEST));
         // Kitchen~
-        putRoom(new EmptyRoom(this, new Point(3,0),"Kitchen"));
+        putRoom(new EmptyRoom(this, new Point(3,0),"Kitchen").setDirections(NORTH,SOUTH,WEST));
         // Sideroom
-        putRoom(new EmptyRoom(this, new Point(1,1),"SideRoom"));
+        putRoom(new EmptyRoom(this, new Point(1,1),"SideRoom").setDirections(EAST,SOUTH));
         // Basement~
-        putRoom(new EmptyRoom(this, new Point(-1,1),"Basement"));
+        putRoom(new EmptyRoom(this, new Point(-1,1),"Basement").setDirections(NORTH));
         // Ghostroom~
-        putRoom(new EmptyRoom(this, new Point(2,2),"Ghostroom"));
+        putRoom(new EmptyRoom(this, new Point(2,2),"Ghostroom").setDirections(SOUTH));
         // Office~
-        putRoom(new EmptyRoom(this, new Point(2,1),"Office"));
+        putRoom(new EmptyRoom(this, new Point(2,1),"Office").setDirections(NORTH,EAST,WEST));
         // Bathroom~
-        putRoom(new EmptyRoom(this, new Point(2,-1),"Bathroom"));
-        // Dark Room~
-        putRoom(new EmptyRoom(this, new Point(3,1),"Dark Room"));
-        // Dining Room
-        putRoom(new EmptyRoom(this, new Point(3,-1),"Room"));
+        putRoom(new EmptyRoom(this, new Point(2,-1),"Bathroom").setDirections(NORTH,EAST));
+        // Darkroom~
+        putRoom(new EmptyRoom(this, new Point(3,1),"Dark Room").setDirections(SOUTH,WEST));
+        // Sideroom2~
+        putRoom(new EmptyRoom(this, new Point(3,-1),"SideRoom2").setDirections(NORTH,WEST));
                 
         currentRoom = map.get(new Point(0, 0));
         facade.onRoomEnter(currentRoom);
