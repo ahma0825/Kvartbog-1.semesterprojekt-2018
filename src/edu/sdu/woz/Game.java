c8f0c436bea0b5bc88e0457e63ebe7549d1cc38c
kvinkel <kvinkel@hotmail.com>
2018-11-06 12:24:19 +0100

    Update descriptions

--- src/edu/sdu/woz/Game.java
+++ src/edu/sdu/woz/Game.java
@@ -21,20 +21,20 @@ public class Game {
 
         // Entrance
         putRoom(new BasicRoom(this, new Point(0, 0),
-                "You find yourself standing in a grand entrance. There's a hallway to the north."
+                "You find yourself standing in a grand entrance."
         ).setDirections(EAST, WEST));
         // End Room~
-        putRoom(new BasicRoom(this, new Point(-1, 0), "End Room").setDirections(EAST));
+        putRoom(new BasicRoom(this, new Point(-1, 0), "You exit the mansion, you are free!").setDirections(EAST));
         // Dining Room~
-        putRoom(new BasicRoom(this, new Point(1, 0), "Dining Room").setDirections(NORTH, EAST, SOUTH, WEST));
+        putRoom(new BasicRoom(this, new Point(1, 0), "You enter a dining hall, there's a feast going on. All the participants are SKELETONS").setDirections(NORTH, EAST, SOUTH, WEST));
         // Room
-        putRoom(new BasicRoom(this, new Point(2, 0), "Room").setDirections(EAST, SOUTH, WEST));
+        putRoom(new BasicRoom(this, new Point(2, 0), "This room is empty").setDirections(EAST, SOUTH, WEST));
         // Kitchen~
-        putRoom(new BasicRoom(this, new Point(3, 0), "Kitchen").setDirections(NORTH, SOUTH, WEST));
+        putRoom(new BasicRoom(this, new Point(3, 0), "You enter the kitchen, knives and plates are all over the place and covered in blood. \nThe only thing that isn't red is a string of garlic.").setDirections(NORTH, SOUTH, WEST));
         // Sideroom
-        putRoom(new BasicRoom(this, new Point(1, 1), "SideRoom").setDirections(EAST, SOUTH));
+        putRoom(new BasicRoom(this, new Point(1, 1), "This room is empty").setDirections(EAST, SOUTH));
         // Basement~
-        putRoom(new BasicRoom(this, new Point(-1, 1), "Basement").setDirections(NORTH));
+        putRoom(new BasicRoom(this, new Point(-1, 1), "The door is heavy and squeaky, there's a strong smell of iron in the air. \nA VAMPIRE APPEARS").setDirections(NORTH));
         // Ghostroom~
         putRoom(new BasicRoom(this, new Point(2, 2), "Ghostroom").setDirections(SOUTH));
         // Office~
@@ -42,9 +42,9 @@ public class Game {
         // Bathroom~
         putRoom(new BasicRoom(this, new Point(2, -1), "Bathroom").setDirections(NORTH, EAST));
         // Darkroom~
-        putRoom(new BasicRoom(this, new Point(3, 1), "Darkroom").setDirections(SOUTH, WEST));
+        putRoom(new BasicRoom(this, new Point(3, 1), "BATS fly out as you enter and the room is dark, moist and covered in cobwebs").setDirections(SOUTH, WEST));
         // Ritualroom~
-        putRoom(new BasicRoom(this, new Point(3, -1), "Ritualroom").setDirections(NORTH, WEST));
+        putRoom(new BasicRoom(this, new Point(3, -1), "You enter a dark room, there are weird markings on the wall. \nThere's a dead body on what appears to be a sacrifice table").setDirections(NORTH, WEST));
 
         currentRoom = map.get(new Point(0, 0));
         facade.onRoomEnter(currentRoom);
