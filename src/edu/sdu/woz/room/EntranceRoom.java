49ce2f61c82b17e42ba2afd2c533a3b1fe907b38
kvinkel <kvinkel@hotmail.com>
2018-11-13 12:22:15 +0100

    Add EntranceRoom

---
+++ src/edu/sdu/woz/room/EntranceRoom.java
@@ -0,0 +1,34 @@
+package edu.sdu.woz.room;
+
+import edu.sdu.woz.Game;
+import edu.sdu.woz.Item;
+import static edu.sdu.woz.Item.KEY;
+import static edu.sdu.woz.Item.SHOTGUN;
+import static edu.sdu.woz.text.Escapes.GO_SCARY;
+import static edu.sdu.woz.text.Escapes.RESET;
+import java.awt.Point;
+
+public class EntranceRoom extends Room {
+
+    public EntranceRoom(Game game, Point pos) {
+        super(game, pos);
+    }
+
+    @Override
+    public String examine() {
+        if (game.getInventory().contains(KEY) && game.getInventory().contains(SHOTGUN)) {
+            return "You enter the grand entrance there's something different."
+                    + "Two yellow glowing eyes appear in the dark corner of the room."
+                    + "Suddenly a " + GO_SCARY + "WEREWOLF jumps out at you!"
+                    + "You whip out the trusted double barrel shotgun and blow it to smithereens";
+        } else if (game.getInventory().contains(KEY) && !game.getInventory().contains(SHOTGUN)){
+            return "You enter the grand entrance there's something different."
+                    + "Two yellow glowing eyes appear in the dark corner of the room."
+                    + "Suddenly a " + GO_SCARY + "WEREWOLF " + RESET + "jumps out at you!"
+                    + "It rips you apart!";
+        } else {
+            return "....";
+        }
+    }
+
+}
