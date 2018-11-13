aeaabb5666dd1bcf4f185d8be70a137a719d591b
Jesper Isgaard Hansen <43139456+IbberSnitte@users.noreply.github.com>
2018-11-13 12:13:31 +0100

    Add Game Over

--- src/edu/sdu/woz/room/BasementRoom.java
+++ src/edu/sdu/woz/room/BasementRoom.java
@@ -27,20 +27,26 @@ public class BasementRoom extends Room {
                 return "The door is heavy and squeaky, there's a strong smell of iron in the air."
                         + "\nA VAMPIRE APPEARS!"
                         + "\nThe vampire jumps at you, bites your neck and starts drinking your blood."
-                        + "\nYou end up motionless on the floor, dying as the vampire sucks the life from you."
-                        + "\nGAME OVER";
+                        + "\nYou end up motionless on the floor, dying as the vampire sucks the life from you.";
                 //Insert game end
             } else {
                 return "The door is heavy and squeaky, there's a strong smell of iron in the air."
                         + "\nA VAMPIRE APPEARS!"
                         + "\nThe vampire jumps for you, but is deterred by the garlic you're carrying."
                         + "\nIt turns into a bat and flies away, dropping a key onto the ground as its form shifts."
-                        + "\nThe only way out of this basement is back north, up the stairs.";
+                        + "\nThe only way out of this basement is back up the stairs.";
                 //Possibly remove vampire when walking in again
             }
         } else{
             return "You walk down into the basement again. "
                     +"\nIt's quite damp, but there's nothing down here.";
+        }
+    }
+    
+    @Override
+    public void afterEnter() {
+        if (!game.getInventory().contains(Item.GARLIC)) {
+            game.gameOver();
         }
     }
 
