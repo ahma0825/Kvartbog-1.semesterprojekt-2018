9119a4ddac706782b744e9c4bcec528df804fb89
Jesper Isgaard Hansen <43139456+IbberSnitte@users.noreply.github.com>
2018-11-13 14:08:52 +0100

    remove template

--- src/edu/sdu/woz/room/OfficeRoom.java
+++ src/edu/sdu/woz/room/OfficeRoom.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.sdu.woz.room;
 
 import edu.sdu.woz.Game;
@@ -12,10 +7,6 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.awt.*;
 
-/**
- *
- * @author jesperisgaard
- */
 public class OfficeRoom extends Room {
 
     private static final String SCREECH1 = "/screech.wav";
