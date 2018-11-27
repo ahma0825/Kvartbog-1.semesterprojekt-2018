8259ffc1e94fadf98bd8b43abef44abe34ac861c
Frederik Mikkelsen <fred@frederikam.com>
2018-11-27 15:44:15 +0100

    Office and ritual buttons

--- src/edu/sdu/woz/gui/SpecialButtonController.java
+++ src/edu/sdu/woz/gui/SpecialButtonController.java
@@ -2,6 +2,10 @@ package edu.sdu.woz.gui;
 
 import edu.sdu.woz.Direction;
 import edu.sdu.woz.Game;
+import edu.sdu.woz.Item;
+import edu.sdu.woz.room.OfficeRoom;
+import edu.sdu.woz.room.RitualRoom;
+import edu.sdu.woz.room.Room;
 import javafx.scene.control.Button;
 
 class SpecialButtonController {
@@ -21,20 +25,25 @@ class SpecialButtonController {
     }
 
     void update() {
-        if (game.getCurrentRoom().canGo(Direction.DOWN)) {
-            specialButton.setVisible(true);
+        Room room = game.getCurrentRoom();
+        specialButton.setVisible(true);
+        if (room.canGo(Direction.DOWN)) {
             specialButton.setText("Go down");
             currentAction = () -> game.go(Direction.DOWN);
-            return;
-        } else if (game.getCurrentRoom().canGo(Direction.UP)) {
-            specialButton.setVisible(true);
+        } else if (room.canGo(Direction.UP)) {
             specialButton.setText("Go up");
             currentAction = () -> game.go(Direction.UP);
-            return;
+        } else if (room instanceof OfficeRoom) {
+            specialButton.setText("Answer phone");
+            currentAction = ((OfficeRoom) room)::answer;
+        } else if (room instanceof RitualRoom && game.getInventory().contains(Item.LIGHTER)) {
+            specialButton.setText("Ignite");
+            currentAction = ((RitualRoom) room)::ignite;
+        } else {
+            specialButton.setVisible(false);
+            currentAction = null;
         }
 
-        specialButton.setVisible(false);
-        currentAction = null;
     }
 
 }
